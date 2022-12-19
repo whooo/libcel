93578042ea2066edf04c009e5cf1c02f71a3ed71
Erik Larsson <who+github@cnackers.org>
2022-12-19 23:26:58 +0100

    json: add support for JSON encoding
    Signed-off-by: Erik Larsson <who+github@cnackers.org>

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 C library to handle the Canonical Event Log format as specified by https://trustedcomputinggroup.org/resource/canonical-event-log-format/
 
 # future work
-* Implement JSON and CBOR formats
+* Implement CBOR format
 * Find public UEFI and IMA logs for use with unit tests
 * Get in contact with specification authors for suggestions/clarifications
---
+++ src/cel_strings.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2022 by Erik Larsson
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include <tss2/tss2_tpm2_types.h>
+#include "cel_types.h"
+
+#ifndef _CEL_STRINGS_H_
+
+#define MAX_DIGEST_HEX TPM2_SHA512_DIGEST_SIZE * 2
+
+const char *
+alg_to_str(TPM2_ALG_ID alg);
+
+CEL_RC
+str_to_alg(const char *str, TPM2_ALG_ID *dest);
+
+CEL_RC
+hexlify(const uint8_t *src, size_t src_len, char *dst, size_t dst_len);
+
+CEL_RC
+unhexlify(const char *src, uint8_t *dst, size_t dst_len);
+
+const char *
+pcclient_event_to_str(uint32_t event_type);
+
+CEL_RC
+str_to_pcclient_event(const char *str, uint32_t *dest);
+
+const char *
+mgt_type_to_str(CEL_TYPE type);
+
+CEL_RC
+str_to_mgt_type(const char *str, TPMI_CELMGTTYPE *dest);
+
+const char *
+content_type_to_str(CEL_TYPE type);
+
+CEL_RC
+str_to_content_type(const char *str, CEL_TYPE *dest);
+
+const char *
+state_trans_to_str(CEL_TYPE trans);
+
+CEL_RC
+str_to_state_trans(const char *str, TPMI_STATE_TRANS *dest);
+
+#define _CEL_STRINGS_H_
+#endif
